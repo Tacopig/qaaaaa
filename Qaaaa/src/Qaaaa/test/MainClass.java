033329c440b6b55311e7b64e810582cd7ebdf4d3
user <user@PC234-14>
2015-10-15 19:52:01 +0800

    hello

---
+++ Qaaaa/src/Qaaaa/test/MainClass.java
@@ -0,0 +1,13 @@
+package Qaaaa.test;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("q543245325");
+	}
+
+}
